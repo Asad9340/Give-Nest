7d4fd55a13ddc41c35cd7226c913b68a1a51fbe8
Asad9340 <asadulimran1999@gmail.com>
2024-03-25 12:50:09 +0600

    show all data to a card

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,8 +1,20 @@
+import { Button } from '@material-tailwind/react';
+
 function Card({ card }) {
-  console.log(card)
+  console.log(card);
+  const { image, title, category, textColor } = card;
+  console.log(textColor)
   return (
-    <div>Card</div>
-  )
+    <div className="border rounded-lg space-y-4">
+      <div>
+        <img className="rounded-t-lg w-full" src={image} alt="" />
+      </div>
+      <div className="space-y-20">
+        <Button>{category}</Button>
+        <h2 className={`text-4xl text-[${textColor}]`}>{title}</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Card
+export default Card;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,15 +1,15 @@
 import useDonationData from "../../Hooks/useDonationData"
-import Card from "./Card";
+import Card from './Card';
 
 function Cards() {
   const { data } = useDonationData();
   return (
-    <div>
-      {
-        data.map((card, index) => <Card key={index} card={card} />)
-      }
+    <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4 my-6 md:my-10">
+      {data.map((card, index) => (
+        <Card key={index} card={card} />
+      ))}
     </div>
-  )
+  );
 }
 
 export default Cards
