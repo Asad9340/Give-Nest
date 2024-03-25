d365976fb1f14b05ab1f6649180cd58aaa5212ca
Asad9340 <asadulimran1999@gmail.com>
2024-03-25 18:28:58 +0600

    responsive for mobile device

--- src/Pages/Donation/DonationPage.jsx
+++ src/Pages/Donation/DonationPage.jsx
@@ -1,19 +1,19 @@
 function DonationPage({ item }) {
   const { image, title, category, categoryBg, textColor, price, cardBg } = item;
   return (
-    <div className="flex gap-4 rounded-lg" style={{backgroundColor:cardBg}}>
+    <div className="flex flex-col md:flex-row gap-4 rounded-lg m-4" style={{backgroundColor:cardBg}}>
       <div>
         <img src={image} alt="" />
       </div>
-      <div className="flex flex-col justify-center space-y-2">
+      <div className="flex flex-col justify-center space-y-2 p-2">
         <button
           className=" py-1 rounded-lg"
           style={{ color: textColor, backgroundColor: categoryBg }}
         >
           {category}
         </button>
-        <h2>{title}</h2>
-        <p>${price}</p>
+        <h2 className="text-xl font-medium">{title}</h2>
+        <p className="text-lg">${price}</p>
         <button
           className="px-4 py-1 rounded-lg font-medium"
           style={{ backgroundColor: categoryBg }}
